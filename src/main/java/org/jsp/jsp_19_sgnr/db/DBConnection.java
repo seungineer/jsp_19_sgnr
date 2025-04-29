98e1bc71ba0e32944112659afa6b522b36131efb
seungineer <97353541+seungineer@users.noreply.github.com>
2025-04-29 09:47:27 +0900

    feat: OracleCloudDB 연결

--- src/main/java/org/jsp/jsp_19_sgnr/db/DBConnection.java
+++ src/main/java/org/jsp/jsp_19_sgnr/db/DBConnection.java
@@ -1,4 +1,23 @@
 package org.jsp.jsp_19_sgnr.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:oracle:thin:@dinkdb_medium?TNS_ADMIN=/Users/mbp/Downloads/Wallet_DinkDB";
+    private static final String USER = "DA2516";
+    private static final String PASSWORD = "Data2516";
+
+    static {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver"); // 드라이버 로딩
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
