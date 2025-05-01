4e50b3d14f3d4625f1debd60a0de997e9b421cdf
seungineer <97353541+seungineer@users.noreply.github.com>
2025-05-01 16:21:20 +0900

    feat: Hikari 커넥션 풀 적용

--- src/main/java/org/jsp/jsp_19_sgnr/db/DBConnection.java
+++ src/main/java/org/jsp/jsp_19_sgnr/db/DBConnection.java
@@ -1,23 +1,30 @@
 package org.jsp.jsp_19_sgnr.db;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:oracle:thin:@dinkdb_medium?TNS_ADMIN=/Users/mbp/Downloads/Wallet_DinkDB";
-    private static final String USER = "DA2516";
-    private static final String PASSWORD = "Data2516";
+    private static final HikariDataSource dataSource;
 
     static {
-        try {
-            Class.forName("oracle.jdbc.OracleDriver"); // 드라이버 로딩
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:oracle:thin:@dinkdb_medium?TNS_ADMIN=/Users/mbp/Downloads/Wallet_DinkDB");
+        config.setUsername("DA2516");
+        config.setPassword("Data2516");
+        config.setDriverClassName("oracle.jdbc.OracleDriver");
+        config.setMaximumPoolSize(3);
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(10000);
+
+        dataSource = new HikariDataSource(config);
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return dataSource.getConnection();
     }
 }
