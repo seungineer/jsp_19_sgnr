424fb0731b53b10cae877de8371b1eaaf63a8e04
seungineer <97353541+seungineer@users.noreply.github.com>
2025-05-02 13:49:25 +0900

    feat: 로그인 시 입력 값 검증(클라이언트, 서버)

--- src/main/java/org/jsp/jsp_19_sgnr/servlet/LoginOk.java
+++ src/main/java/org/jsp/jsp_19_sgnr/servlet/LoginOk.java
@@ -9,9 +9,12 @@ import org.jsp.jsp_19_sgnr.dto.Member;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
+import java.util.regex.Pattern;
 
 @WebServlet("/loginOk")
 public class LoginOk extends HttpServlet {
+    private final Pattern emailPattern = Pattern.compile("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
+    private final Pattern pwPattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{5,15}$");
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -23,6 +26,12 @@ public class LoginOk extends HttpServlet {
         String paswd = request.getParameter("paswd");
         String rememberId = request.getParameter("rememberId");
 
+        if (id == null || paswd == null || !emailPattern.matcher(id).matches() || !pwPattern.matcher(paswd).matches()) {
+            String errorMessage = "아이디 또는 비밀번호가 잘못되었습니다.";
+            String encodedMessage = URLEncoder.encode(errorMessage, "UTF-8");
+            response.sendRedirect("login.html?error=" + encodedMessage);
+            return;
+        }
         MemberDao memberDao = new MemberDao();
         Member member = memberDao.findByIdAndPswd(id, paswd);
 
